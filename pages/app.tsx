6c54475d6c861ae0df1c6d3cb2c1ce07eaa49258
TemitopeGX <temitopeayomikun999@gmail.com>
2025-01-16 01:25:53 +0100

    Remove redundant app.tsx file

--- pages/app.tsx
+++
@@ -1,9 +0,0 @@
-
-import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
-import styles from '../styles/Home.module.css';
-
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
